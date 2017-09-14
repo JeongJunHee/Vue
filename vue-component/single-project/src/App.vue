f19d3b16fdcfb5f189779558b3c89ac9aa0b3153
준희-PC\Jun <unizet1@gmail.com>
2017-09-14 15:23:43 +0900

    component

---
+++ vue-component/single-project/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <login></login> -->
+    <Register></Register>
+    <!-- <stories></stories> -->
+    <!-- <famous></famous> -->
+  </div>
+</template>
+
+<script>
+// import Login from './components/Login.vue'
+import Register from './components/Register'
+// import Stories from './components/Stories'
+// import Famous from './components/Famous'
+
+export default {
+  name: 'app',
+  components: {
+    // Login,
+    Register
+    // Stories,
+    // Famous
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ vue-component/single-project/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="register">
+      <h2>Register Form</h2>
+      <input placeholder="First Name" class="form-control">
+      <input placeholder="Last Name" class="form-control">
+      <input placeholder="Email address" class="form-control">
+      <input placeholder="Pick a password" class="form-control">
+      <input placeholder="Confirm password" class="form-control">
+      <button class="btn btn-primary">Sign up</button>
+      <famous></famous>
+    </div>
+</template>
+
+<script>
+import Famous from './Famous.vue'
+
+export default {
+  components: {
+    Famous
+  },
+  created () {
+    console.log('register')
+  }
+}
+</script>
+
+<style scoped>
+input {
+  margin-top: 10px;
+}
+
+button {
+  margin-top: 10px;
+  width: 100%;
+}
+
+</style>
